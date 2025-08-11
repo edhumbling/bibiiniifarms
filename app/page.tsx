a618d999a54ddd80babd9e1c75c85c7ff0741fca
Emmanuel D. Humbling <emmergraix@gmail.com>
2025-08-11 15:26:26 +0000

    Add poster fallback for hero video and preload/preconnect for instant start

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
             loop
             playsInline
             preload="auto"
+            poster="https://ik.imagekit.io/humbling/reve_images_2025-08-04_14-48-38/reve_images_2025-08-05_03-59-11/4811dfb5-4d98-4ba5-9088-236d082bca94.png"
           />
           <div className="absolute inset-0 bg-black/30"></div>
         </div>
