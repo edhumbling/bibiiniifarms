9cdbf303b6dd4b39d455b53783a2634ac749da1e
Emmanuel D. Humbling <emmergraix@gmail.com>
2025-08-12 06:40:43 +0000

    Integrate Sentry for error tracking and update configuration settings in Next.js project

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6bdc0e010d58035ece6e51551dfa2726@o4509829550964736.ingest.us.sentry.io/4509829552144384",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
